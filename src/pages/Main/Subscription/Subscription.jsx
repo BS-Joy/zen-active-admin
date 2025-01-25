f4f434618a9da6a66447568a65c5c2f37bbe3b56
Raiyan109 <kraiyan109@gmail.com>
2025-01-25 14:40:23 +0600

    subscription bg and colors changed

--- src/pages/Main/Subscription/Subscription.jsx
+++ src/pages/Main/Subscription/Subscription.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react"
-import tickImg from '../../../assets/images/membership-tick.png'
+import tickImg from '../../../assets/images/check-circle.png'
+import tickImgWhite from '../../../assets/images/check-circle-white.png'
 import { FaPlus } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 import AddNewButton from "../../../Components/AddNewButton";
 import Swal from "sweetalert2";
 
 const plans = [
-    { id: 1, name: 'Workout Membership', price: 0, features: ['View Members Directory', 'View Members Profile', 'Send Private Messages', 'Add Media To Your Profile'] },
-    { id: 2, name: 'Nutrition Membership ', price: 20, features: ['View Members Directory', 'View Members Profile', 'Send Private Messages', 'Add Media To Your Profile'] },
-    { id: 3, name: 'Nutrition Membership', price: 60, features: ['Discuss your program', 'Track your progress by scheduling calls periodically', 'Accountability', ' life style coaching'] },
+    { id: 1, name: '1month Free Trial', price: 0, features: ['Personalized meal plans', 'Adaptive fitness recommendations', 'Offline access', 'Premium challenges and rewards'], period: 'monthly', isRecommended: false },
+    { id: 2, name: 'Monthly Subscription', price: 20, features: ['Personalized meal plans', 'Adaptive fitness recommendations', 'Premium challenges and rewards', 'Daily Energy Expenditure Calculator', 'Mood Tracking', 'Emotional Support', 'Offline access', 'Ad-free experience'], period: 'monthly', isRecommended: true },
+    { id: 3, name: 'Annual Subscription', price: 79.99, features: ['Everything in Monthly Plan', 'Save 50% with annual Plan'], period: 'yearly', isRecommended: false },
 ]
 const Subscription = () => {
     const [isLoading, setIsLoading] = useState(false)
@@ -85,25 +86,25 @@ const Subscription = () => {
                 <div className='flex items-center justify-center gap-12'>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
                         {plans.map((plan) => (
-                            <div key={plan.id} className="bg-white rounded-xl w-64 py-6">
-                                <div className="flex flex-col items-center justify-center text-[#EB4B73] gap-2 pb-5 border-b-2 border-b-[#EB4B73]/20 min-h-[130px]">
-                                    <h1 className="text-xl text-center">{plan.name}</h1>
-                                    <div className="flex flex-col items-center">
-                                        <div className="flex items-center text-xl">
+                            <div key={plan.id} className={`${plan.isRecommended ? 'bg-[#1E648C]' : 'bg-white'} rounded-xl w-[352px] py-6`}>
+                                <div className={`flex flex-col items-center justify-center  gap-2 pb-5 border-b-2 ${plan.isRecommended ? 'border-b-[#EBF8FF]/20' : 'border-b-[#2781B5]/20'}  min-h-[117px]`}>
+                                    <h1 className={`text-xl font-semibold text-center ${plan.isRecommended ? 'text-[#FBFDFD]/90' : 'text-[#174C6B]'}`}>{plan.name}</h1>
+                                    <div className="flex items-center">
+                                        <div className={`flex items-center text-lg ${plan.isRecommended ? 'text-[#FBFDFD]/90' : 'text-[#2781B5]'} `}>
                                             <span>{plan.price === 0 ? '' : '$'}</span>
                                             <h2>{plan.price === 0 ? 'Free' : plan.price}</h2>
                                         </div>
-                                        <span className="text-xs">{plan.price === 0 ? '' : '/month'}</span>
+                                        <span className={`text-sm ${plan.isRecommended ? 'text-[#FBFDFD]/90' : 'text-[#2781B5]'}`}>{plan.price === 0 ? '' : plan.period === 'yearly' ? '/year' : '/month'}</span>
                                     </div>
                                 </div>
-                                <div className="flex items-center justify-center py-6">
+                                <div className="flex flex-col items-center justify-center gap-8 py-6">
                                     <div className="space-y-5 flex flex-col items-start justify-center">
                                         {plan.features.map((option, i) => (
                                             <div key={i} className="flex items-start gap-3">
-                                                <img src={tickImg}
+                                                <img src={plan.isRecommended ? tickImgWhite : tickImg}
                                                     alt='Tick image' className="w-5 h-5" />
 
-                                                <h3 className="text-xs max-w-[160px]">{option}</h3>
+                                                <h3 className={`text-[16px] ${plan.isRecommended ? 'text-[#FBFDFD]/90' : 'text-[#525252]'}  font-semibold`}>{option}</h3>
                                             </div>
                                         ))}
                                         {/* {plan.options.map((option, i) => (
@@ -116,11 +117,12 @@ const Subscription = () => {
                                             />
                                             <h3 className="text-xs">{option}</h3>
                                         </div>
-                                    ))} */}
-                                        <div className="flex justify-center items-center gap-3">
-                                            <button className="px-6 py-1.5 min-w-[100px] text-center text-[#174C6B] bg-[#EBF8FF] border border-[#174C6B] rounded-md active:text-[#174C6B] hover:bg-transparent hover:text-[#174C6B] focus:outline-none focus:ring" onClick={handleDelete} >Delete</button>
-                                            <button className="px-6 py-1.5 min-w-[100px] text-center text-white bg-[#174C6B] border border-[#174C6B] rounded-md active:text-[#174C6B] hover:bg-transparent hover:text-[#174C6B] focus:outline-none focus:ring" onClick={() => navigate("/edit-subscription")}>Edit</button>
-                                        </div>
+                                        ))} */}
+
+                                    </div>
+                                    <div className="flex  items-center gap-3">
+                                        <button className="px-12 py-2.5 w-full text-center text-lg text-[#174C6B] bg-[#EBF8FF] border border-[#174C6B] rounded-md active:text-[#174C6B] hover:bg-transparent hover:text-[#174C6B] focus:outline-none focus:ring" onClick={handleDelete} >Delete</button>
+                                        <button className={`px-12 py-2.5 w-full text-center text-lg text-white bg-[#174C6B] border border-[#174C6B] rounded-md active:text-[#174C6B] hover:bg-transparent hover:text-[#174C6B] focus:outline-none focus:ring`} onClick={() => navigate("/edit-subscription")}>Edit</button>
                                     </div>
                                 </div>
                             </div>
