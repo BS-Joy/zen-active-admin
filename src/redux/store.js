9186aaa5dad5b1dc4b2577a97f791f5003f0f3c6
Raiyan109 <kraiyan109@gmail.com>
2025-02-10 12:53:06 +0600

    baseApi done

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { baseApi } from './api/baseApi'
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        [baseApi.reducerPath]: baseApi.reducer,
+    },
+    middleware: (getDefaultMiddlewares) => getDefaultMiddlewares().concat(baseApi.middleware)
 })
