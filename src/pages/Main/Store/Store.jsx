fc54b791663a0a12cc44615e9b5e27ce1e7ba66c
Raiyan109 <kraiyan109@gmail.com>
2025-02-13 15:03:17 +0600

    workout video started

--- src/pages/Main/Store/Store.jsx
+++ src/pages/Main/Store/Store.jsx
@@ -119,7 +119,7 @@ const Store = () => {
                 <FaPlus />
                 Add Badge</button>
             <div className="py-10">
-                <div className="rounded-lg border-2 py-4 border-[#174C6B]/80 mt-8 recent-users-table">
+                <div className="rounded-lg border-2 py-4 border-[#37B5FF]/80 mt-8 recent-users-table">
                     <div className="flex justify-between px-2">
                         <h3 className="text-2xl font-semibold text-black mb-4 pl-2">Badges List</h3>
                         <div className="flex items-center gap-4 mb-6">
