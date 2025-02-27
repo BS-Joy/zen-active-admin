2146b676c1d2237deb9742770239973be0ca4685
Raiyan109 <kraiyan109@gmail.com>
2025-02-27 11:43:31 +0600

    chage password page done

--- src/pages/Main/Setting/Change-password/ChangePassword.jsx
+++ src/pages/Main/Setting/Change-password/ChangePassword.jsx
@@ -1,7 +1,7 @@
 import { FaArrowLeft, FaEye, FaLock } from "react-icons/fa6";
-import { MdLockOutline } from "react-icons/md";
-import { FaRegEyeSlash } from "react-icons/fa";
+import { HiOutlineEyeSlash } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
+import { HiOutlineLockClosed } from "react-icons/hi";
 
 const ChangePassword = () => {
     const navigate = useNavigate();
@@ -11,12 +11,16 @@ const ChangePassword = () => {
     return (
         <div className="flex items-center justify-center ">
             <div className="bg-[#F4F9FB] rounded-lg shadow-lg mt-8 w-[610px] h-[725px] mx-auto py-10 px-8">
-                <div className="flex flex-col  w-full max-w-md mx-auto mt-10 p-4 rounded-lg space-y-4">
-                    <div className="flex items-center gap-2 cursor-pointer" onClick={handleBackButtonClick}>
-                        <FaArrowLeft />
-                        <h1>Change password</h1>
+                <div className="flex flex-col w-full max-w-md mx-auto mt-10 p-4 rounded-lg">
+                    <div className="space-y-[10px] mb-[30px]">
+                        <div className="flex items-center gap-2 cursor-pointer" onClick={handleBackButtonClick}>
+                            <div className="w-[24px] h-[24px]">
+                                <FaArrowLeft className="text-[19px]" />
+                            </div>
+                            <h1 className="text-[26px] text-[#525252] font-semibold">Change password</h1>
+                        </div>
+                        <h1 className="text-[20px] text-[#525252] font-semibold">Your password must be 8-10 character long.</h1>
                     </div>
-                    <h1>Your password must be 8-10 character long.</h1>
                     {/* Input Fields */}
                     <div className="flex flex-col w-full space-y-4">
                         {[
@@ -25,31 +29,35 @@ const ChangePassword = () => {
                             { label: 'Re-enter new password', placeholder: 'Re-enter new password' },
                         ].map(({ label, placeholder }, index) => (
                             <div>
-                                <h1 className="mb-3">{label}</h1>
+                                <h1 className="mb-[5px] text-[20px] text-[#4B4B4B] font-semibold">{label}</h1>
                                 <div key={index} className="relative flex items-center">
                                     {/* Lock Icon */}
-                                    <MdLockOutline className="absolute left-3 text-[#174C6B]" />
+                                    <div className="absolute left-3 text-[#2781B5] w-[20px] h-[20px]">
+                                        <HiOutlineLockClosed size={19} className="" />
+                                    </div>
                                     {/* Input Field */}
                                     <input
                                         type="password"
                                         placeholder={placeholder}
-                                        className="w-full pl-10 pr-10 py-2 border border-black rounded-lg placeholder:text-black focus:outline-none focus:ring-2 focus:ring-gray-400"
+                                        className="w-[487px] pl-10 pr-10 h-[54px] pt-[3px] border border-[#79CDFF] rounded-lg placeholder:text-[16px] placeholder:font-semibold placeholder:text-[#757575] focus:outline-none focus:ring-2 focus:ring-gray-400"
                                     />
                                     {/* Eye Icon */}
-                                    <FaRegEyeSlash className="absolute right-3  cursor-pointer" />
+                                    <div className="w-[20px] h-[20px]">
+                                        <HiOutlineEyeSlash className="absolute right-8  cursor-pointer  font-bold" size={15} />
+                                    </div>
                                 </div>
                             </div>
                         ))}
                     </div>
 
                     {/* Forgot Password */}
-                    <p className="mt-4 text-sm text-[#174C6B] ml-auto cursor-pointer hover:underline" onClick={(e) => navigate(`forgot-password`)}>
+                    <p className="mt-[12px] text-[16px] text-[#2781B5] font-semibold ml-auto mr-4 cursor-pointer hover:underline" onClick={(e) => navigate(`forgot-password`)}>
                         Forgot Password?
                     </p>
 
                     {/* Update Password Button */}
-                    <button className="mt-6 w-full bg-[#174C6B] text-white py-2 rounded-lg hover:bg-[#174C6B]/80">
-                        Update Password
+                    <button className="mt-6 w-full bg-[#174C6B] text-white h-[56px] rounded-lg hover:bg-[#174C6B]/80 text-[24px] pt-1">
+                        Update password
                     </button>
                 </div>
             </div>
