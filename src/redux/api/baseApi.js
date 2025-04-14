55f2816af28547a6f49a13475623d311017e07bd
Badhan Samadder <badhansamadder5@gmail.com>
2025-04-14 10:11:14 +0600

    api base url changed

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://192.168.10.18:3500/api",
+    baseUrl: import.meta.env.VITE_BASE_URL,
+    // baseUrl: "http://192.168.10.18:3500/api",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
 
